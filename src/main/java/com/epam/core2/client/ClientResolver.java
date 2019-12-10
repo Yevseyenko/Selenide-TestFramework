5f8ac7245e727f9f3f171508701a84930786eb38
andrii.valkovskyi <andrii.valkovskyi@thomsonreuters.com>
2019-12-10 11:43:17 +0200

    4

--- src/main/java/com/epam/core2/client/ClientResolver.java
+++ src/main/java/com/epam/core2/client/ClientResolver.java
@@ -2,6 +2,7 @@ package com.epam.core2.client;
 //TODO fix core2, all names should have information under it
 
 public abstract class ClientResolver {
+    //now replace out RestAssured with appache with minimal changes and take note what could be done better from the start
    public static InterfaceClient getClient(String dataProviderClassName){
        if(dataProviderClassName.equals("NameDataProvider")){
            return new RestAssuredClient();
--- src/main/java/com/epam/core2/client/InterfaceClient.java
+++ src/main/java/com/epam/core2/client/InterfaceClient.java
@@ -3,6 +3,8 @@ package com.epam.core2.client;
 import com.epam.core2.models.User;
 
 public interface InterfaceClient<T> {
+
+    //do you have IDEA showing you a problem here?
 public int getUsersStatusCode();
 public int getCreateUserStatusCode(User user);
 public String getUserByFirstNameResponse(String userName);
